fc6742bea7df20f5589a248dbb3da43e41a5bd5b
Juank <148532589+ErjuanK@users.noreply.github.com>
2026-03-11 22:49:11 +0100

    Act titulo Pagina

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>R E C I C L E D ☆ — @yield('title', 'Tu Música')</title>
     <link rel="shortcut icon" href="{{ asset('multimedia/img/logo_3d.ico') }}" type="image/x-icon">
     
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
